50bfd1695be1405d00ace6c0546cfb967cc28942
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-06-25 16:34:06 +0700

    update code (#14)
    * create yml file to deploy to azure
    
    * update workflows
    
    * Using @Accessor lombok
    
    * Update rabbitmq

--- src/main/java/com/example/springsocial/payload/AuthResponse.java
+++ src/main/java/com/example/springsocial/payload/AuthResponse.java
@@ -1,26 +1,15 @@
 package com.example.springsocial.payload;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
 public class AuthResponse {
   private String accessToken;
   private String tokenType = "Bearer";
 
   public AuthResponse(String accessToken) {
     this.accessToken = accessToken;
-  }
-
-  public String getAccessToken() {
-    return accessToken;
-  }
-
-  public void setAccessToken(String accessToken) {
-    this.accessToken = accessToken;
-  }
-
-  public String getTokenType() {
-    return tokenType;
-  }
-
-  public void setTokenType(String tokenType) {
-    this.tokenType = tokenType;
   }
 }
