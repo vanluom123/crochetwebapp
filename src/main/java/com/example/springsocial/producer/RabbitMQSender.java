b7362531f2a1152fd2e178b6c9df58be981ccc22
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-06-26 21:05:40 +0700

    Make document for function (#16)

--- src/main/java/com/example/springsocial/producer/RabbitMQSender.java
+++ src/main/java/com/example/springsocial/producer/RabbitMQSender.java
@@ -17,6 +17,13 @@ public class RabbitMQSender {
   private final RabbitMQProperties rabbitProps;
   private final Channel channel;
 
+  /**
+   * Constructs a new RabbitMQSender object.
+   *
+   * @param amqpTemplate  The AmqpTemplate object for sending messages to RabbitMQ.
+   * @param rabbitProps   The RabbitMQProperties object containing RabbitMQ configuration.
+   * @param channel       The Channel object for interacting with RabbitMQ.
+   */
   @Autowired
   public RabbitMQSender(AmqpTemplate amqpTemplate, RabbitMQProperties rabbitProps, Channel channel) {
     this.amqpTemplate = amqpTemplate;
@@ -24,23 +31,48 @@ public class RabbitMQSender {
     this.channel = channel;
   }
 
+
+  /**
+   * Converts the given message of type T and produces it to the specified RabbitMQ exchange with the provided routing key.
+   *
+   * @param message The message to be converted and produced.
+   * @param <T> The type of the message.
+   */
   public <T> void convertAndProduce(T message) {
+    // Convert and send the message using AMQP template
     amqpTemplate.convertAndSend(rabbitProps.getExchange(), rabbitProps.getRoutingkey(), message);
+
+    // Log sent message
     log.info("Send msg = " + message);
   }
 
+
+  /**
+   * Publishes a message to RabbitMQ.
+   *
+   * @param message The message to be published.
+   * @throws RuntimeException If an error occurs during message publishing.
+   */
   public void produce(String message) {
     try {
-      produce(message, rabbitProps.getExchange(), rabbitProps.getQueue(), rabbitProps.getRoutingkey());
+      declareExchangeAndQueue();
+      channel.basicPublish(rabbitProps.getExchange(), rabbitProps.getRoutingkey(), null, message.getBytes());
+      log.info("Sent message: {}", message);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private void produce(String message, String exchange, String queueName, String routingKey) throws IOException {
-    channel.exchangeDeclare(exchange, BuiltinExchangeType.DIRECT);
-    channel.queueDeclare(queueName, true, false, false, null);
-    channel.queueBind(queueName, exchange, routingKey, null);
-    channel.basicPublish(exchange, routingKey, null, message.getBytes());
+
+  /**
+   * Declares the exchange, queue, and binding in RabbitMQ.
+   *
+   * @throws IOException If an error occurs during the declaration process.
+   */
+  private void declareExchangeAndQueue() throws IOException {
+    channel.exchangeDeclare(rabbitProps.getExchange(), BuiltinExchangeType.DIRECT, true);
+    channel.queueDeclare(rabbitProps.getQueue(), true, false, false, null);
+    channel.queueBind(rabbitProps.getQueue(), rabbitProps.getExchange(), rabbitProps.getRoutingkey());
   }
+
 }
