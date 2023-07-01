435ecb44cdf7d7db2271f804cf9c599b5ba7f07d
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-07-01 15:40:50 +0700

    Only login social with Google (#21)
    * Remove social login with GitHub and Facebook
    * Add document
    * Update processOAuth2User

--- src/main/java/com/example/springsocial/security/TokenAuthenticationFilter.java
+++ src/main/java/com/example/springsocial/security/TokenAuthenticationFilter.java
@@ -63,11 +63,24 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
   }
 
 
+  /**
+   * Retrieves the JWT (JSON Web Token) from the "Authorization" header of an HTTP request.
+   *
+   * @param request The HttpServletRequest representing the HTTP request.
+   * @return The extracted JWT, or null if it is not found.
+   */
   private String getJwtFromRequest(HttpServletRequest request) {
+    // Retrieve the value of the "Authorization" header from the request
     String bearerToken = request.getHeader("Authorization");
+
+    // Check if the "Authorization" header value is not empty and starts with "Bearer "
     if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+      // Extract and return the JWT by removing the "Bearer " prefix
       return bearerToken.substring(7);
     }
+
+    // Return null if the JWT is not found or the "Authorization" header is missing or malformed
     return null;
   }
+
 }
