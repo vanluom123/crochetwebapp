a1ebe8fcf7aac6c5750d58456efc6699991ea720
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-08-14 16:02:14 +0700

    Add cookie for jwt token (#37)

--- src/main/java/org/crochet/config/SecurityConfig.java
+++ src/main/java/org/crochet/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
