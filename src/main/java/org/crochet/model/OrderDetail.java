fa53db6e2a82be8b5050d3538769b4a6a698e3cc
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-10-15 18:11:39 +0700

    create blog post (#40)
    Create a blog post

--- src/main/java/org/crochet/model/OrderDetail.java
+++ src/main/java/org/crochet/model/OrderDetail.java
@@ -1,7 +1,9 @@
 package org.crochet.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,11 +29,11 @@ public class OrderDetail {
   @Column(name = "id", nullable = false)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
   @JoinColumn(name = "pattern_id")
   private Pattern pattern;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
   @JoinColumn(name = "order_id")
   private Order order;
 }
---
+++ src/main/java/org/crochet/service/BlogPostServiceImpl.java
@@ -0,0 +1,82 @@
+package org.crochet.service;
+
+import org.crochet.exception.ResourceNotFoundException;
+import org.crochet.mapper.BlogPostMapper;
+import org.crochet.model.BlogPost;
+import org.crochet.repository.BlogPostRepository;
+import org.crochet.repository.BlogPostSpecifications;
+import org.crochet.request.BlogPostRequest;
+import org.crochet.response.BlogPostPaginationResponse;
+import org.crochet.response.BlogPostResponse;
+import org.crochet.service.abstraction.BlogPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class BlogPostServiceImpl implements BlogPostService {
+
+  @Autowired
+  private BlogPostRepository blogPostRepository;
+
+  @Transactional
+  @Override
+  public void createOrUpdatePost(BlogPostRequest request) {
+    var blogPost = blogPostRepository.findById(request.getId()).orElse(null);
+    if (blogPost == null) {
+      // create new a post
+      blogPost = BlogPost.builder()
+          .id(request.getId())
+          .title(request.getTitle())
+          .content(request.getContent())
+          .imageUrl(request.getImageUrl())
+          .creationDate(LocalDateTime.now())
+          .build();
+    } else {
+      // update the post
+      blogPost.setTitle(request.getTitle());
+      blogPost.setContent(request.getContent());
+      blogPost.setImageUrl(request.getImageUrl());
+    }
+
+    blogPost = blogPostRepository.save(blogPost);
+  }
+
+  @Override
+  public BlogPostPaginationResponse getBlogs(int pageNo, int pageSize, String sortBy, String sortDir, String text) {
+    // create Sort instance
+    Sort sort = Sort.by(sortBy);
+    sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? sort.ascending() : sort.descending();
+    // create Pageable instance
+    Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+    Specification<BlogPost> spec = Specification.where(null);
+    if (text != null && !text.isEmpty()) {
+      spec = spec.and(BlogPostSpecifications.searchBy(text));
+    }
+
+    Page<BlogPost> menuPage = blogPostRepository.findAll(spec, pageable);
+    var contents = BlogPostMapper.INSTANCE.toResponses(menuPage.getContent());
+
+    return BlogPostPaginationResponse.builder()
+        .contents(contents)
+        .pageNo(menuPage.getNumber())
+        .totalElements(menuPage.getTotalElements())
+        .totalPages(menuPage.getTotalPages())
+        .pageSize(menuPage.getSize())
+        .last(menuPage.isLast())
+        .build();
+  }
+
+  @Override
+  public BlogPostResponse getDetail(long id) {
+    var blogPost = blogPostRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Blog not found"));
+    return BlogPostMapper.INSTANCE.toResponse(blogPost);
+  }
+}
