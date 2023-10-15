5a39b9c9a0fbf7fd185cccf7b62e0fb06e71b800
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-10-15 18:11:39 +0700

    create blog post (#40)

---
+++ src/main/java/org/crochet/response/BlogPostResponse.java
@@ -0,0 +1,20 @@
+package org.crochet.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BlogPostResponse {
+  private Long id;
+  private String title;
+  private String content;
+  private String imageUrl;
+  private LocalDateTime creationDate;
+}
