ba4976e758535e44c88b9d1128c412083e2991b7
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-11-22 20:11:13 +0700

    Create comment (#42)
    * Create comment

---
+++ src/main/java/org/crochet/controller/CommentController.java
@@ -0,0 +1,26 @@
+package org.crochet.controller;
+
+import org.crochet.request.CommentRequest;
+import org.crochet.service.abstraction.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+
+  private final CommentService commentService;
+
+  public CommentController(CommentService commentService) {
+    this.commentService = commentService;
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<String> createComment(@RequestBody CommentRequest request) {
+    commentService.createOrUpdate(request);
+    return ResponseEntity.ok("Create comment successfully");
+  }
+}
