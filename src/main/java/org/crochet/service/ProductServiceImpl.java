0bce6688a122f131e7e48bccdbe144646a6497e4
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-12-06 22:51:39 +0700

    Remove Firebase service (#47)
    * Remove Firebase service
    
    * Remove unused

--- src/main/java/org/crochet/service/ProductServiceImpl.java
+++ src/main/java/org/crochet/service/ProductServiceImpl.java
@@ -10,7 +10,6 @@ import org.crochet.request.ProductRequest;
 import org.crochet.response.ProductPaginationResponse;
 import org.crochet.response.ProductResponse;
 import org.crochet.service.contact.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
