27d0c85b72abba01e53bae7b204b0281df921e57
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-12-10 21:34:32 +0700

    Fix home controller (#52)
    * Init database
    * Update ApiExceptionHandler
    * Update mapper
    * Update responses
    
    (cherry picked from commit e098beceb7173fd7a34222c9122404a4693ef39c)

--- src/main/java/org/crochet/mapper/BlogPostMapper.java
+++ src/main/java/org/crochet/mapper/BlogPostMapper.java
@@ -1,20 +1,40 @@
 package org.crochet.mapper;
 
+import org.crochet.model.BlogFile;
 import org.crochet.model.BlogPost;
 import org.crochet.response.BlogPostResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BlogPostMapper {
     BlogPostMapper INSTANCE = Mappers.getMapper(BlogPostMapper.class);
 
+    @Mapping(target = "id", source = "id", qualifiedByName = "uuidToString")
+    @Mapping(target = "bytes", source = "blogFiles", qualifiedByName = "toList")
     BlogPostResponse toResponse(BlogPost blogPost);
+
+    @Named("toList")
+    default List<String> toList(Collection<BlogFile> blogFiles) {
+        return Optional.ofNullable(blogFiles)
+                .map(blog -> blog.stream()
+                        .map(BlogFile::getBytes)
+                        .toList())
+                .orElse(null);
+    }
+
+    @Named("uuidToString")
+    default String uuidToString(UUID uuid) {
+        return uuid.toString();
+    }
 
     default List<BlogPostResponse> toResponses(Collection<BlogPost> blogPosts) {
         return Optional.ofNullable(blogPosts)
