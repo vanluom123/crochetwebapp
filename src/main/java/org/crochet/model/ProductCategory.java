bf435ce1efa66786dc2db412965c2e5b1a3ef6e5
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-12-22 11:42:23 +0700

    Some changes (#54)
    * Remove ProductFile, PatternFile, FreePatternFile, and BlogFile
    * Using @ElementCollection instead

--- src/main/java/org/crochet/model/ProductCategory.java
+++ src/main/java/org/crochet/model/ProductCategory.java
@@ -1,6 +1,13 @@
 package org.crochet.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
