505340e56f715a0d69ed71eef4a48aef9ac87a58
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2023-12-22 11:42:23 +0700

    Some changes (#54)
    * Remove ProductFile, PatternFile, FreePatternFile, and BlogFile
    * Using @ElementCollection instead

--- src/main/java/org/crochet/controller/FreePatternController.java
+++ src/main/java/org/crochet/controller/FreePatternController.java
@@ -15,10 +15,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/free-pattern")
@@ -31,33 +34,34 @@ public class FreePatternController {
 
     @Operation(summary = "Create a pattern")
     @ApiResponse(responseCode = "200", description = "Pattern created successfully",
-                 content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
-    @PostMapping("/create")
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+    @PostMapping(value = "/create")
     @PreAuthorize("hasRole('ADMIN')")
     @SecurityRequirement(name = "BearerAuth")
     public ResponseEntity<String> createPattern(
-            @RequestBody FreePatternRequest request) {
-        freePatternService.createOrUpdate(request);
-        return ResponseEntity.ok("Create pattern successfully");
+            @RequestPart FreePatternRequest request,
+            @RequestPart List<MultipartFile> files) {
+        var result = freePatternService.createOrUpdate(request, files);
+        return ResponseEntity.ok(result);
     }
 
     @Operation(summary = "Get paginated list of patterns")
     @ApiResponse(responseCode = "200", description = "List of patterns",
-                 content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = PaginatedFreePatternResponse.class)))
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = PaginatedFreePatternResponse.class)))
     @GetMapping("/pagination")
     public ResponseEntity<PaginatedFreePatternResponse> getPatterns(
             @Parameter(description = "Page number (default: 0)")
             @RequestParam(value = "pageNo", defaultValue = AppConstant.DEFAULT_PAGE_NUMBER,
-                          required = false) int pageNo,
+                    required = false) int pageNo,
             @Parameter(description = "Page size (default: 10)")
             @RequestParam(value = "pageSize", defaultValue = AppConstant.DEFAULT_PAGE_SIZE,
-                          required = false) int pageSize,
+                    required = false) int pageSize,
             @Parameter(description = "Sort by field (default: id)")
             @RequestParam(value = "sortBy", defaultValue = AppConstant.DEFAULT_SORT_BY, required = false) String sortBy,
             @Parameter(description = "Sort direction (default: ASC)")
             @RequestParam(value = "sortDir", defaultValue = AppConstant.DEFAULT_SORT_DIRECTION,
-                          required = false) String sortDir,
+                    required = false) String sortDir,
             @Parameter(description = "Search text")
             @RequestParam(value = "text", required = false) String text) {
         var response = freePatternService.getFreePatterns(pageNo, pageSize, sortBy, sortDir, text);
@@ -66,8 +70,8 @@ public class FreePatternController {
 
     @Operation(summary = "Get pattern details by ID")
     @ApiResponse(responseCode = "200", description = "Pattern details retrieved successfully",
-                 content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = FreePatternResponse.class)))
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = FreePatternResponse.class)))
     @GetMapping("/detail")
     public ResponseEntity<FreePatternResponse> getDetail(
             @Parameter(description = "ID of the pattern to retrieve")
--- src/main/java/org/crochet/model/Pattern.java
+++ src/main/java/org/crochet/model/Pattern.java
@@ -1,8 +1,10 @@
 package org.crochet.model;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,7 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import java.util.Set;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,6 +25,9 @@ public class Pattern extends BasePattern {
     @Column(name = "price")
     private double price;
 
-    @OneToMany(mappedBy = "pattern")
-    private Set<PatternFile> patternFiles;
+    @ElementCollection
+    @CollectionTable(name = "pattern_file",
+            joinColumns = {@JoinColumn(name = "pattern_id", nullable = false)})
+    @Column(name = "file_name", columnDefinition = "LONGBLOB")
+    private List<String> files;
 }
