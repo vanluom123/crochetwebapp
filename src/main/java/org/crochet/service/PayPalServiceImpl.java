ac9c672d554badaa20d8db187eaa0e59082c126b
Phan Van Luom <phanvanluom97bd@gmail.com>
2023-12-27 22:36:28 +0700

    Fix pipeline build failed

--- src/main/java/org/crochet/service/PayPalServiceImpl.java
+++ src/main/java/org/crochet/service/PayPalServiceImpl.java
@@ -1,51 +1,66 @@
 package org.crochet.service;
 
 import com.google.gson.Gson;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.crochet.payload.dto.OrderDTO;
 import org.crochet.service.contact.PayPalService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.xiaofeng.webclient.service.WebClientService;
-import org.xiaofeng.webclient.type.HttpMethod;
+
+import java.io.IOException;
 
 @Service
 public class PayPalServiceImpl implements PayPalService {
-    @Value("${paypal.username:username not found}")
-    private String username;
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-    @Value("${paypal.password:password not found}")
-    private String password;
-
-    private final WebClientService webClientService;
     private final Gson gson;
+    private final OkHttpClient client;
 
-    public PayPalServiceImpl(WebClientService webClientService, Gson gson) {
-        this.webClientService = webClientService;
+    public PayPalServiceImpl(Gson gson, OkHttpClient client) {
         this.gson = gson;
+        this.client = client;
     }
 
     @Override
     public String createOrder(OrderDTO orderDTO) {
         String payload = gson.toJson(orderDTO);
         String uri = "https://api-m.sandbox.paypal.com/v2/checkout/orders";
-        return webClientService.invokeApi(uri,
-                HttpMethod.POST,
-                payload,
-                header -> {
-                    header.setBasicAuth(username, password);
-                    header.setContentType(MediaType.APPLICATION_JSON);
-                }).block();
+        RequestBody requestBody = RequestBody.create(payload, JSON);
+        Request request = new Request.Builder()
+                .url(uri)
+                .post(requestBody)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            ResponseBody responseBody = response.body();
+            if (responseBody != null) {
+                return responseBody.string();
+            }
+            return null;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String capturePayment(String orderId) {
         String uri = "https://api-m.sandbox.paypal.com/v2/checkout/orders/" + orderId + "/capture";
-        return webClientService.invokeApi(uri,
-                HttpMethod.POST,
-                header -> {
-                    header.setBasicAuth(username, password);
-                    header.setContentType(MediaType.APPLICATION_JSON);
-                }).block();
+        RequestBody requestBody = RequestBody.create(JSON, new byte[0]);
+        Request request = new Request.Builder()
+                .url(uri)
+                .post(requestBody)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            ResponseBody responseBody = response.body();
+            if (responseBody != null) {
+                return responseBody.string();
+            }
+            return null;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
