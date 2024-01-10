1a3f0fa869ea40c279b0262c57327e582743d45a
Charles Phan <32066691+vanluom123@users.noreply.github.com>
2024-01-10 15:03:41 +0700

    Update AuthResponse after successful login (#63)

--- src/main/java/org/crochet/service/contact/UserService.java
+++ src/main/java/org/crochet/service/contact/UserService.java
@@ -3,10 +3,14 @@ package org.crochet.service.contact;
 import org.crochet.model.User;
 import org.crochet.payload.request.SignUpRequest;
 
+import java.util.UUID;
+
 public interface UserService {
     User createUser(SignUpRequest signUpRequest);
 
     User getByEmail(String email);
+
+    User getById(UUID id);
 
     void updatePassword(String password, String email);
 
