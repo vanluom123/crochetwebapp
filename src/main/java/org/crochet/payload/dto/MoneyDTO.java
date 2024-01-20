23b2c2d1485c9680353e5c096af8d075e1994c32
Phan Van Luom <phanvanluom97bd@gmail.com>
2024-01-20 20:40:08 +0700

    Jackson instead of Gson

--- src/main/java/org/crochet/payload/dto/MoneyDTO.java
+++ src/main/java/org/crochet/payload/dto/MoneyDTO.java
@@ -1,13 +1,13 @@
 package org.crochet.payload.dto;
 
-import com.google.gson.annotations.SerializedName;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class MoneyDTO {
-    @SerializedName("currency_code")
+    @JsonProperty("currency_code")
     private String currencyCode;
     private String value;
 }
