2f20bf5536fd1bafeb0f5bf038e70f62c396299f
Phan Van Luom <phanvanluom97bd@gmail.com>
2024-01-20 17:22:59 +0700

    Configure WebClient

--- src/main/java/org/crochet/exception/ApiError.java
+++ src/main/java/org/crochet/exception/ApiError.java
@@ -2,11 +2,11 @@ package org.crochet.exception;
 
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 @Data
 @Builder
 public class ApiError {
     private String message;
-    private HttpStatus statusCode;
+    private int code;
+    private String error;
 }
