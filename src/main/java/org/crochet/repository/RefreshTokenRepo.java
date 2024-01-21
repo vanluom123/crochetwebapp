5bfbcb2801adef7e1d4c54f8714c2ae614322b1e
Phan Van Luom <phanvanluom97bd@gmail.com>
2024-01-21 17:48:51 +0700

    Fixing refresh token

---
+++ src/main/java/org/crochet/repository/RefreshTokenRepo.java
@@ -0,0 +1,24 @@
+package org.crochet.repository;
+
+import org.crochet.model.RefreshToken;
+import org.crochet.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRepo extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByToken(String token);
+    @Query("""
+                select r
+                from RefreshToken r
+                where r.user = ?1
+                and r.expiryDate > current_timestamp
+                and r.revoked = false
+            """)
+    List<RefreshToken> findAllValidRefreshTokenByUser(User user);
+}
