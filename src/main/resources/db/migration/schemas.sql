608cb5f26c0921550dd1dd224b1ba2f30df6c86a
Phan Van Luom <phanvanluom97bd@gmail.com>
2024-01-23 23:19:32 +0700

    Add Azure Blob Storage

--- src/main/resources/db/migration/schemas.sql
+++ src/main/resources/db/migration/schemas.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset luompv97:1705942231293-1
+-- changeset luompv97:1706060531065-1
 CREATE TABLE blog_post
 (
     id            BINARY(16) NOT NULL,
@@ -10,22 +10,23 @@ CREATE TABLE blog_post
     CONSTRAINT pk_blog_post PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-2
+-- changeset luompv97:1706060531065-2
 CREATE TABLE blog_post_file
 (
     blog_post_id BINARY(16) NOT NULL,
     file_name    LONGBLOB NULL
 );
 
--- changeset luompv97:1705942231293-3
-CREATE TABLE category_lv1
+-- changeset luompv97:1706060531065-3
+CREATE TABLE category
 (
-    id   BINARY(16) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_category_lv1 PRIMARY KEY (id)
+    id        BINARY(16) NOT NULL,
+    name      VARCHAR(255) NOT NULL,
+    parent_id BINARY(16) NULL,
+    CONSTRAINT pk_category PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-4
+-- changeset luompv97:1706060531065-4
 CREATE TABLE comment
 (
     id           BINARY(16) NOT NULL,
@@ -36,7 +37,7 @@ CREATE TABLE comment
     CONSTRAINT pk_comment PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-5
+-- changeset luompv97:1706060531065-5
 CREATE TABLE confirmation_token
 (
     id           BINARY(16) NOT NULL,
@@ -48,7 +49,7 @@ CREATE TABLE confirmation_token
     CONSTRAINT pk_confirmation_token PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-6
+-- changeset luompv97:1706060531065-6
 CREATE TABLE free_pattern
 (
     id            BINARY(16) NOT NULL,
@@ -57,14 +58,14 @@ CREATE TABLE free_pattern
     CONSTRAINT pk_free_pattern PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-7
+-- changeset luompv97:1706060531065-7
 CREATE TABLE free_pattern_file
 (
     free_pattern_id BINARY(16) NOT NULL,
     file_name       LONGBLOB NULL
 );
 
--- changeset luompv97:1705942231293-8
+-- changeset luompv97:1706060531065-8
 CREATE TABLE order_pattern_detail
 (
     id             BINARY(16) NOT NULL,
@@ -76,7 +77,7 @@ CREATE TABLE order_pattern_detail
     CONSTRAINT pk_order_pattern_detail PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-9
+-- changeset luompv97:1706060531065-9
 CREATE TABLE orders
 (
     id      BINARY(16) NOT NULL,
@@ -84,7 +85,7 @@ CREATE TABLE orders
     CONSTRAINT pk_orders PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-10
+-- changeset luompv97:1706060531065-10
 CREATE TABLE password_reset_token
 (
     id         BINARY(16) NOT NULL,
@@ -95,7 +96,7 @@ CREATE TABLE password_reset_token
     CONSTRAINT pk_password_reset_token PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-11
+-- changeset luompv97:1706060531065-11
 CREATE TABLE pattern
 (
     id            BINARY(16) NOT NULL,
@@ -106,33 +107,33 @@ CREATE TABLE pattern
     CONSTRAINT pk_pattern PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-12
+-- changeset luompv97:1706060531065-12
 CREATE TABLE pattern_file
 (
     pattern_id BINARY(16) NOT NULL,
     file_name  LONGBLOB NULL
 );
 
--- changeset luompv97:1705942231293-13
+-- changeset luompv97:1706060531065-13
 CREATE TABLE product
 (
-    id              BINARY(16) NOT NULL,
-    name            VARCHAR(255) NULL,
-    `description`   LONGBLOB NULL,
+    id            BINARY(16) NOT NULL,
+    name          VARCHAR(255) NULL,
+    `description` LONGBLOB NULL,
     price DOUBLE DEFAULT 0 NOT NULL,
-    currency_code   VARCHAR(20) DEFAULT 'USD' NOT NULL,
-    category_lv1_id BINARY(16) NOT NULL,
+    currency_code VARCHAR(20) DEFAULT 'USD' NOT NULL,
+    category_id   BINARY(16) NOT NULL,
     CONSTRAINT pk_product PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-14
+-- changeset luompv97:1706060531065-14
 CREATE TABLE product_file
 (
     product_id BINARY(16) NOT NULL,
     file_name  LONGBLOB NULL
 );
 
--- changeset luompv97:1705942231293-15
+-- changeset luompv97:1706060531065-15
 CREATE TABLE refresh_token
 (
     id          BINARY(16) NOT NULL,
@@ -143,7 +144,7 @@ CREATE TABLE refresh_token
     CONSTRAINT pk_refresh_token PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-16
+-- changeset luompv97:1706060531065-16
 CREATE TABLE users
 (
     id                BINARY(16) NOT NULL,
@@ -159,71 +160,75 @@ CREATE TABLE users
     CONSTRAINT pk_users PRIMARY KEY (id)
 );
 
--- changeset luompv97:1705942231293-17
+-- changeset luompv97:1706060531065-17
 ALTER TABLE users
     ADD CONSTRAINT uc_74165e195b2f7b25de690d14a UNIQUE (email);
 
--- changeset luompv97:1705942231293-18
-ALTER TABLE category_lv1
-    ADD CONSTRAINT uc_category_lv1_name UNIQUE (name);
+-- changeset luompv97:1706060531065-18
+ALTER TABLE category
+    ADD CONSTRAINT uc_category_name UNIQUE (name);
 
--- changeset luompv97:1705942231293-19
+-- changeset luompv97:1706060531065-19
 ALTER TABLE order_pattern_detail
     ADD CONSTRAINT uc_order_pattern_detail_transaction UNIQUE (transaction_id);
 
--- changeset luompv97:1705942231293-20
+-- changeset luompv97:1706060531065-20
 ALTER TABLE refresh_token
     ADD CONSTRAINT uc_refresh_token_token UNIQUE (token);
 
--- changeset luompv97:1705942231293-21
+-- changeset luompv97:1706060531065-21
+ALTER TABLE category
+    ADD CONSTRAINT FK_CATEGORY_ON_PARENT FOREIGN KEY (parent_id) REFERENCES category (id);
+
+-- changeset luompv97:1706060531065-22
 ALTER TABLE comment
     ADD CONSTRAINT FK_COMMENT_ON_POST FOREIGN KEY (post_id) REFERENCES blog_post (id);
 
--- changeset luompv97:1705942231293-22
+-- changeset luompv97:1706060531065-23
 ALTER TABLE comment
     ADD CONSTRAINT FK_COMMENT_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset luompv97:1705942231293-23
+-- changeset luompv97:1706060531065-24
 ALTER TABLE confirmation_token
     ADD CONSTRAINT FK_CONFIRMATION_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset luompv97:1705942231293-24
+-- changeset luompv97:1706060531065-25
 ALTER TABLE orders
     ADD CONSTRAINT FK_ORDERS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset luompv97:1705942231293-25
+-- changeset luompv97:1706060531065-26
 ALTER TABLE order_pattern_detail
     ADD CONSTRAINT FK_ORDER_PATTERN_DETAIL_ON_ORDER FOREIGN KEY (order_id) REFERENCES orders (id);
 
--- changeset luompv97:1705942231293-26
+-- changeset luompv97:1706060531065-27
 ALTER TABLE order_pattern_detail
     ADD CONSTRAINT FK_ORDER_PATTERN_DETAIL_ON_PATTERN FOREIGN KEY (pattern_id) REFERENCES pattern (id);
 
--- changeset luompv97:1705942231293-27
+-- changeset luompv97:1706060531065-28
 ALTER TABLE password_reset_token
     ADD CONSTRAINT FK_PASSWORD_RESET_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset luompv97:1705942231293-28
+-- changeset luompv97:1706060531065-29
 ALTER TABLE product
-    ADD CONSTRAINT FK_PRODUCT_ON_CATEGORY_LV1 FOREIGN KEY (category_lv1_id) REFERENCES category_lv1 (id);
+    ADD CONSTRAINT FK_PRODUCT_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES category (id);
 
--- changeset luompv97:1705942231293-29
+-- changeset luompv97:1706060531065-30
 ALTER TABLE refresh_token
     ADD CONSTRAINT FK_REFRESH_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset luompv97:1705942231293-30
+-- changeset luompv97:1706060531065-31
 ALTER TABLE blog_post_file
     ADD CONSTRAINT fk_blog_post_file_on_blog_post FOREIGN KEY (blog_post_id) REFERENCES blog_post (id);
 
--- changeset luompv97:1705942231293-31
+-- changeset luompv97:1706060531065-32
 ALTER TABLE free_pattern_file
     ADD CONSTRAINT fk_free_pattern_file_on_free_pattern FOREIGN KEY (free_pattern_id) REFERENCES free_pattern (id);
 
--- changeset luompv97:1705942231293-32
+-- changeset luompv97:1706060531065-33
 ALTER TABLE pattern_file
     ADD CONSTRAINT fk_pattern_file_on_pattern FOREIGN KEY (pattern_id) REFERENCES pattern (id);
 
--- changeset luompv97:1705942231293-33
+-- changeset luompv97:1706060531065-34
 ALTER TABLE product_file
     ADD CONSTRAINT fk_product_file_on_product FOREIGN KEY (product_id) REFERENCES product (id);
 
