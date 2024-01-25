517b37a2519b937554638db440b9f4aa14cb810b
Charles Phan <phanvanluom97bd@gmail.com>
2024-01-25 13:22:12 +0700

    Update security configuration and Firebase storage controller

--- src/main/java/org/crochet/controller/FirebaseStorageController.java
+++ src/main/java/org/crochet/controller/FirebaseStorageController.java
@@ -3,8 +3,10 @@ package org.crochet.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.crochet.service.contact.FirebaseStorageService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -14,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/firebase-storage")
+@RequestMapping("/firebase-storage")
 public class FirebaseStorageController {
     private final FirebaseStorageService firebaseStorageService;
 
@@ -24,6 +26,8 @@ public class FirebaseStorageController {
 
     @Operation(summary = "Upload multiple files to Firebase Cloud Storage")
     @ApiResponse(responseCode = "200", description = "Upload files successfully", content = @Content(mediaType = "application/json"))
+    @PreAuthorize("hasRole('ADMIN')")
+    @SecurityRequirement(name = "BearerAuth")
     @PostMapping(value = "/upload-file", consumes = {"multipart/form-data"})
     public ResponseEntity<List<String>> uploadMultipleFiles(@RequestPart MultipartFile[] files) {
         var fileUrls = firebaseStorageService.uploadMultipleFiles(files);
