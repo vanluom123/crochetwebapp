56f5897053ae9d33567df5c71cc421f1cf6fb2ca
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-15 00:14:13 +0700

    Migration pattern and free pattern

--- src/main/resources/db/migration/schemas.sql
+++ src/main/resources/db/migration/schemas.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset vanluom123:1710358566881-1
+-- changeset vanluom123:1710435474402-1
 CREATE TABLE blog_post
 (
     id            BINARY(16)   NOT NULL,
@@ -10,14 +10,14 @@ CREATE TABLE blog_post
     CONSTRAINT pk_blog_post PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-2
+-- changeset vanluom123:1710435474402-2
 CREATE TABLE blog_post_file
 (
     blog_post_id BINARY(16) NOT NULL,
     file_name    LONGBLOB   NULL
 );
 
--- changeset vanluom123:1710358566881-3
+-- changeset vanluom123:1710435474402-3
 CREATE TABLE category
 (
     id        BINARY(16)   NOT NULL,
@@ -26,7 +26,25 @@ CREATE TABLE category
     CONSTRAINT pk_category PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-4
+-- changeset vanluom123:1710435474402-4
+CREATE TABLE category_free_pattern
+(
+    id        BINARY(16)   NOT NULL,
+    name      VARCHAR(255) NOT NULL,
+    parent_id BINARY(16)   NULL,
+    CONSTRAINT pk_category_free_pattern PRIMARY KEY (id)
+);
+
+-- changeset vanluom123:1710435474402-5
+CREATE TABLE category_pattern
+(
+    id        BINARY(16)   NOT NULL,
+    name      VARCHAR(255) NOT NULL,
+    parent_id BINARY(16)   NULL,
+    CONSTRAINT pk_category_pattern PRIMARY KEY (id)
+);
+
+-- changeset vanluom123:1710435474402-6
 CREATE TABLE comment
 (
     id           BINARY(16) NOT NULL,
@@ -37,7 +55,7 @@ CREATE TABLE comment
     CONSTRAINT pk_comment PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-5
+-- changeset vanluom123:1710435474402-7
 CREATE TABLE confirmation_token
 (
     id           BINARY(16)   NOT NULL,
@@ -49,24 +67,32 @@ CREATE TABLE confirmation_token
     CONSTRAINT pk_confirmation_token PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-6
+-- changeset vanluom123:1710435474402-8
 CREATE TABLE free_pattern
 (
-    id            BINARY(16)   NOT NULL,
-    name          VARCHAR(255) NULL,
-    `description` LONGBLOB     NULL,
-    author        VARCHAR(255) NULL,
+    id                       BINARY(16)   NOT NULL,
+    name                     VARCHAR(255) NULL,
+    `description`            LONGBLOB     NULL,
+    author                   VARCHAR(255) NULL,
+    category_free_pattern_id BINARY(16)   NOT NULL,
     CONSTRAINT pk_free_pattern PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-7
+-- changeset vanluom123:1710435474402-9
 CREATE TABLE free_pattern_file
 (
     free_pattern_id BINARY(16) NOT NULL,
     file_name       LONGBLOB   NULL
 );
 
--- changeset vanluom123:1710358566881-8
+-- changeset vanluom123:1710435474402-10
+CREATE TABLE free_pattern_photos
+(
+    free_pattern_id BINARY(16) NOT NULL,
+    photo_name      LONGBLOB   NULL
+);
+
+-- changeset vanluom123:1710435474402-11
 CREATE TABLE order_pattern_detail
 (
     id             BINARY(16)   NOT NULL,
@@ -78,7 +104,7 @@ CREATE TABLE order_pattern_detail
     CONSTRAINT pk_order_pattern_detail PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-9
+-- changeset vanluom123:1710435474402-12
 CREATE TABLE orders
 (
     id      BINARY(16) NOT NULL,
@@ -86,7 +112,7 @@ CREATE TABLE orders
     CONSTRAINT pk_orders PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-10
+-- changeset vanluom123:1710435474402-13
 CREATE TABLE password_reset_token
 (
     id         BINARY(16)   NOT NULL,
@@ -97,32 +123,33 @@ CREATE TABLE password_reset_token
     CONSTRAINT pk_password_reset_token PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-11
+-- changeset vanluom123:1710435474402-14
 CREATE TABLE pattern
 (
-    id            BINARY(16)                NOT NULL,
-    name          VARCHAR(255)              NULL,
-    `description` LONGBLOB                  NULL,
-    price         DOUBLE      DEFAULT 0     NOT NULL,
-    currency_code VARCHAR(20) DEFAULT 'USD' NOT NULL,
+    id                  BINARY(16)                NOT NULL,
+    name                VARCHAR(255)              NULL,
+    `description`       LONGBLOB                  NULL,
+    price               DOUBLE      DEFAULT 0     NOT NULL,
+    currency_code       VARCHAR(20) DEFAULT 'USD' NOT NULL,
+    category_pattern_id BINARY(16)                NOT NULL,
     CONSTRAINT pk_pattern PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-12
+-- changeset vanluom123:1710435474402-15
 CREATE TABLE pattern_file
 (
     pattern_id BINARY(16) NOT NULL,
     file_name  LONGBLOB   NULL
 );
 
--- changeset vanluom123:1710358566881-13
-CREATE TABLE photos
+-- changeset vanluom123:1710435474402-16
+CREATE TABLE pattern_photos
 (
-    free_pattern_id BINARY(16) NOT NULL,
-    file_name       LONGBLOB   NULL
+    pattern_id BINARY(16) NOT NULL,
+    photo_name LONGBLOB   NULL
 );
 
--- changeset vanluom123:1710358566881-14
+-- changeset vanluom123:1710435474402-17
 CREATE TABLE product
 (
     id            BINARY(16)                NOT NULL,
@@ -134,14 +161,14 @@ CREATE TABLE product
     CONSTRAINT pk_product PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-15
+-- changeset vanluom123:1710435474402-18
 CREATE TABLE product_file
 (
     product_id BINARY(16) NOT NULL,
     file_name  LONGBLOB   NULL
 );
 
--- changeset vanluom123:1710358566881-16
+-- changeset vanluom123:1710435474402-19
 CREATE TABLE refresh_token
 (
     id          BINARY(16)       NOT NULL,
@@ -152,7 +179,7 @@ CREATE TABLE refresh_token
     CONSTRAINT pk_refresh_token PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-17
+-- changeset vanluom123:1710435474402-20
 CREATE TABLE users
 (
     id                BINARY(16)                  NOT NULL,
@@ -168,79 +195,107 @@ CREATE TABLE users
     CONSTRAINT pk_users PRIMARY KEY (id)
 );
 
--- changeset vanluom123:1710358566881-18
+-- changeset vanluom123:1710435474402-21
 ALTER TABLE users
     ADD CONSTRAINT uc_74165e195b2f7b25de690d14a UNIQUE (email);
 
--- changeset vanluom123:1710358566881-19
+-- changeset vanluom123:1710435474402-22
+ALTER TABLE category_free_pattern
+    ADD CONSTRAINT uc_category_free_pattern_name UNIQUE (name);
+
+-- changeset vanluom123:1710435474402-23
 ALTER TABLE category
     ADD CONSTRAINT uc_category_name UNIQUE (name);
 
--- changeset vanluom123:1710358566881-20
+-- changeset vanluom123:1710435474402-24
+ALTER TABLE category_pattern
+    ADD CONSTRAINT uc_category_pattern_name UNIQUE (name);
+
+-- changeset vanluom123:1710435474402-25
 ALTER TABLE order_pattern_detail
     ADD CONSTRAINT uc_order_pattern_detail_transaction UNIQUE (transaction_id);
 
--- changeset vanluom123:1710358566881-21
+-- changeset vanluom123:1710435474402-26
 ALTER TABLE refresh_token
     ADD CONSTRAINT uc_refresh_token_token UNIQUE (token);
 
--- changeset vanluom123:1710358566881-22
+-- changeset vanluom123:1710435474402-27
+ALTER TABLE category_free_pattern
+    ADD CONSTRAINT FK_CATEGORY_FREE_PATTERN_ON_PARENT FOREIGN KEY (parent_id) REFERENCES category_free_pattern (id);
+
+-- changeset vanluom123:1710435474402-28
 ALTER TABLE category
     ADD CONSTRAINT FK_CATEGORY_ON_PARENT FOREIGN KEY (parent_id) REFERENCES category (id);
 
--- changeset vanluom123:1710358566881-23
+-- changeset vanluom123:1710435474402-29
+ALTER TABLE category_pattern
+    ADD CONSTRAINT FK_CATEGORY_PATTERN_ON_PARENT FOREIGN KEY (parent_id) REFERENCES category_pattern (id);
+
+-- changeset vanluom123:1710435474402-30
 ALTER TABLE comment
     ADD CONSTRAINT FK_COMMENT_ON_POST FOREIGN KEY (post_id) REFERENCES blog_post (id);
 
--- changeset vanluom123:1710358566881-24
+-- changeset vanluom123:1710435474402-31
 ALTER TABLE comment
     ADD CONSTRAINT FK_COMMENT_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset vanluom123:1710358566881-25
+-- changeset vanluom123:1710435474402-32
 ALTER TABLE confirmation_token
     ADD CONSTRAINT FK_CONFIRMATION_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset vanluom123:1710358566881-26
+-- changeset vanluom123:1710435474402-33
+ALTER TABLE free_pattern
+    ADD CONSTRAINT FK_FREE_PATTERN_ON_CATEGORY_FREE_PATTERN FOREIGN KEY (category_free_pattern_id) REFERENCES category_free_pattern (id);
+
+-- changeset vanluom123:1710435474402-34
 ALTER TABLE orders
     ADD CONSTRAINT FK_ORDERS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset vanluom123:1710358566881-27
+-- changeset vanluom123:1710435474402-35
 ALTER TABLE order_pattern_detail
     ADD CONSTRAINT FK_ORDER_PATTERN_DETAIL_ON_ORDER FOREIGN KEY (order_id) REFERENCES orders (id);
 
--- changeset vanluom123:1710358566881-28
+-- changeset vanluom123:1710435474402-36
 ALTER TABLE order_pattern_detail
     ADD CONSTRAINT FK_ORDER_PATTERN_DETAIL_ON_PATTERN FOREIGN KEY (pattern_id) REFERENCES pattern (id);
 
--- changeset vanluom123:1710358566881-29
+-- changeset vanluom123:1710435474402-37
 ALTER TABLE password_reset_token
     ADD CONSTRAINT FK_PASSWORD_RESET_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset vanluom123:1710358566881-30
+-- changeset vanluom123:1710435474402-38
+ALTER TABLE pattern
+    ADD CONSTRAINT FK_PATTERN_ON_CATEGORY_PATTERN FOREIGN KEY (category_pattern_id) REFERENCES category_pattern (id);
+
+-- changeset vanluom123:1710435474402-39
 ALTER TABLE product
     ADD CONSTRAINT FK_PRODUCT_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES category (id);
 
--- changeset vanluom123:1710358566881-31
+-- changeset vanluom123:1710435474402-40
 ALTER TABLE refresh_token
     ADD CONSTRAINT FK_REFRESH_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
--- changeset vanluom123:1710358566881-32
+-- changeset vanluom123:1710435474402-41
 ALTER TABLE blog_post_file
     ADD CONSTRAINT fk_blog_post_file_on_blog_post FOREIGN KEY (blog_post_id) REFERENCES blog_post (id);
 
--- changeset vanluom123:1710358566881-33
+-- changeset vanluom123:1710435474402-42
 ALTER TABLE free_pattern_file
     ADD CONSTRAINT fk_free_pattern_file_on_free_pattern FOREIGN KEY (free_pattern_id) REFERENCES free_pattern (id);
 
--- changeset vanluom123:1710358566881-34
+-- changeset vanluom123:1710435474402-43
+ALTER TABLE free_pattern_photos
+    ADD CONSTRAINT fk_free_pattern_photos_on_free_pattern FOREIGN KEY (free_pattern_id) REFERENCES free_pattern (id);
+
+-- changeset vanluom123:1710435474402-44
 ALTER TABLE pattern_file
     ADD CONSTRAINT fk_pattern_file_on_pattern FOREIGN KEY (pattern_id) REFERENCES pattern (id);
 
--- changeset vanluom123:1710358566881-35
-ALTER TABLE photos
-    ADD CONSTRAINT fk_photos_on_free_pattern FOREIGN KEY (free_pattern_id) REFERENCES free_pattern (id);
+-- changeset vanluom123:1710435474402-45
+ALTER TABLE pattern_photos
+    ADD CONSTRAINT fk_pattern_photos_on_pattern FOREIGN KEY (pattern_id) REFERENCES pattern (id);
 
--- changeset vanluom123:1710358566881-36
+-- changeset vanluom123:1710435474402-46
 ALTER TABLE product_file
     ADD CONSTRAINT fk_product_file_on_product FOREIGN KEY (product_id) REFERENCES product (id);
 
