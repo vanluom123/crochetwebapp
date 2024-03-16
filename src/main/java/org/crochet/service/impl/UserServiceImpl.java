3fd0bd733a6a456f06ffa6e9b5a0f9def1844f98
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-17 01:51:49 +0700

    Add error message constants and use them in exception handling

--- src/main/java/org/crochet/service/impl/UserServiceImpl.java
+++ src/main/java/org/crochet/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.crochet.service.impl;
 
+import org.crochet.constant.MessageConstant;
 import org.crochet.enumerator.AuthProvider;
 import org.crochet.exception.BadRequestException;
 import org.crochet.exception.ResourceNotFoundException;
@@ -11,6 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+
+import static org.crochet.constant.MessageCode.*;
+import static org.crochet.constant.MessageConstant.*;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -26,7 +30,7 @@ public class UserServiceImpl implements UserService {
     public User createUser(SignUpRequest signUpRequest) {
         // Check if the email address is already in use
         if (isValidEmail(signUpRequest.getEmail())) {
-            throw new BadRequestException("Email address already in use");
+            throw new BadRequestException(MessageConstant.EMAIL_ADDRESS_ALREADY_IN_USE_MESSAGE, EMAIL_ADDRESS_ALREADY_IN_USE_CODE);
         }
 
         // Creating user's account
@@ -45,13 +49,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getByEmail(String email) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("User not existed with email " + email));
+                .orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND_WITH_EMAIL_MESSAGE + email, USER_NOT_FOUND_WITH_EMAIL_CODE));
     }
 
     @Override
     public User getById(UUID id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User not existed with id " + id));
+                .orElseThrow(() -> new ResourceNotFoundException(USER_NOT_FOUND_WITH_ID_MESSAGE + id, USER_NOT_FOUND_WITH_ID_CODE));
     }
 
     @Override
@@ -69,7 +73,7 @@ public class UserServiceImpl implements UserService {
         var user = this.getByEmail(email);
         var isMatch = passwordEncoder.matches(password, user.getPassword());
         if (!isMatch) {
-            throw new BadRequestException("Incorrect password");
+            throw new BadRequestException(INCORRECT_PASSWORD_MESSAGE, INCORRECT_PASSWORD_CODE);
         }
         return user;
     }
