0f4fe7ba6f22677d0637487fd567c50b0a65e33a
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-17 01:50:47 +0700

    Remove unused import and method from CategoryRepo

--- src/main/java/org/crochet/repository/CategoryRepo.java
+++ src/main/java/org/crochet/repository/CategoryRepo.java
@@ -2,14 +2,10 @@ package org.crochet.repository;
 
 import org.crochet.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepo extends JpaRepository<Category, UUID> {
     Optional<Category> findByName(String name);
-    @Query("select c from Category c where c.parent.id = ?1")
-    List<Category> findSubCategoriesByParent(UUID parentId);
 }
