dbed8171343b00f4f49e2221ef87dff720bbe211
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-17 01:51:24 +0700

    Refactor file upload and add file deletion functionality

--- src/main/java/org/crochet/service/impl/FirebaseStorageServiceImpl.java
+++ src/main/java/org/crochet/service/impl/FirebaseStorageServiceImpl.java
@@ -3,12 +3,15 @@ package org.crochet.service.impl;
 import com.google.cloud.storage.Blob;
 import com.google.firebase.cloud.StorageClient;
 import lombok.extern.slf4j.Slf4j;
+import org.crochet.payload.response.FileResponse;
 import org.crochet.service.FirebaseStorageService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -21,7 +24,7 @@ public class FirebaseStorageServiceImpl implements FirebaseStorageService {
         this.storageClient = storageClient;
     }
 
-    public String uploadFile(MultipartFile imageFile) {
+    public FileResponse uploadFile(MultipartFile imageFile) {
         // Define the path and filename in Firebase Cloud Storage
         String fileName = "images/" + imageFile.getOriginalFilename();
 
@@ -38,17 +41,39 @@ public class FirebaseStorageServiceImpl implements FirebaseStorageService {
         log.info("image name: {}", blob.getName());
 
         String blobName = blob.getName().replaceFirst("images/", "");
+        String fileContent = "https://firebasestorage.googleapis.com/v0/b/" + BUCKET_NAME + "/o/images%2F" + blobName + "?alt=media";
 
-        return "https://firebasestorage.googleapis.com/v0/b/" + BUCKET_NAME + "/o/images%2F" + blobName + "?alt=media";
+        return new FileResponse(fileName, fileContent);
     }
 
     @Override
-    public List<String> uploadMultipleFiles(MultipartFile[] files) {
-        List<String> fileNames;
+    public List<FileResponse> uploadMultipleFiles(MultipartFile[] files) {
+        List<FileResponse> fileNames;
         fileNames = Stream.of(files)
                 .parallel()
                 .map(this::uploadFile)
                 .toList();
         return fileNames;
+    }
+
+    @Override
+    public List<String> deleteMultipleFiles(List<String> fileNames) {
+        return fileNames.parallelStream()
+                .map(this::tryDeleteFile)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    private Optional<String> tryDeleteFile(String fileName) {
+        Blob blob = storageClient.bucket(BUCKET_NAME).get(fileName);
+        if (blob != null) {
+            blob.delete();
+            log.info("File {} has been deleted", fileName);
+            return Optional.empty();
+        } else {
+            log.error("File {} does not exist", fileName);
+            return Optional.of(fileName);
+        }
     }
 }
