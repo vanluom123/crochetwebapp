de8dcb083bbdb4c3bb6b94220ee78a44fcfd2253
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-17 01:49:22 +0700

    Add FileMapper, ImageMapper, and update other mappers

--- src/main/java/org/crochet/mapper/FreePatternMapper.java
+++ src/main/java/org/crochet/mapper/FreePatternMapper.java
@@ -5,13 +5,15 @@ import org.crochet.payload.response.FreePatternResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        uses = {FileMapper.class, ImageMapper.class})
 public interface FreePatternMapper {
     FreePatternMapper INSTANCE = Mappers.getMapper(FreePatternMapper.class);
 
