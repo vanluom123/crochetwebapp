6703b43ff5016b4244eff8d4c8461607bcfcf314
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-17 01:45:44 +0700

    Add message codes and update message constants

---
+++ src/main/java/org/crochet/constant/MessageCode.java
@@ -0,0 +1,34 @@
+package org.crochet.constant;
+
+public class MessageCode {
+    public static final int CATEGORY_NOT_FOUND_CODE = 1;
+    public static final int USER_NAME_NOT_FOUND_CODE = 1;
+    public static final int EMAIL_NOT_VERIFIED_CODE = 1;
+    public static final int INCORRECT_PASSWORD_CODE = 1;
+    public static final int EMAIL_ADDRESS_ALREADY_IN_USE_CODE = 1;
+    public static final int USER_NOT_FOUND_WITH_EMAIL_CODE = 1;
+    public static final int USER_NOT_FOUND_WITH_ID_CODE = 1;
+    public static final int UNAUTHORIZED_CODE = 1;
+    public static final int FORBIDDEN_CODE = 1;
+    public static final int EMAIL_ALREADY_CONFIRMED_CODE = 1;
+    public static final int TOKEN_EXPIRED_CODE = 1;
+    public static final int PASSWORD_RESET_TOKEN_IS_EXPIRED_CODE = 1;
+    public static final int REFRESH_TOKEN_IS_NOT_IN_DB_CODE = 1;
+    public static final int BLOG_NOT_FOUND_CODE = 1;
+    public static final int PLEASE_LOGIN_TO_COMMENT_CODE = 1;
+    public static final int USER_NOT_FOUND_CODE = 1;
+    public static final int COMMENT_NOT_FOUND_CODE = 1;
+    public static final int CONFIRM_TOKEN_NOT_FOUND_CODE = 1;
+    public static final int FAILED_TO_SEND_EMAIL_CODE = 1;
+    public static final int FREE_PATTERN_NOT_FOUND_CODE = 1;
+    public static final int PATTERN_NOT_FOUND_CODE = 1;
+    public static final int ORDER_PATTERN_DETAIL_NOT_FOUND_FOR_TRANSACTION_ID_CODE = 1;
+    public static final int LOGIN_TO_ORDER_PATTERN_CODE = 1;
+    public static final int USER_NOT_FOUND_WITH_TOKEN_CODE = 1;
+    public static final int PASSWORD_RESET_TOKEN_NOT_FOUND_CODE = 1;
+    public static final int USER_NOT_LOGGED_IN_CODE = 1;
+    public static final int USER_NOT_PAYMENT_FOR_THIS_PATTERN_CODE = 1;
+    public static final int PRODUCT_NOT_FOUND_CODE = 1;
+    public static final int REFRESH_TOKEN_IS_EXPIRED_CODE = 1;
+    public static final int REFRESH_TOKEN_NOT_FOUND_CODE = 1;
+}
