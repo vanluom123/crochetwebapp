fe8ff71e761d9b6ab9fb135429b8147b1362539f
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-17 01:51:32 +0700

    Add getAllCategories method to CategoryService interface

--- src/main/java/org/crochet/service/CategoryService.java
+++ src/main/java/org/crochet/service/CategoryService.java
@@ -19,6 +19,8 @@ public interface CategoryService {
 
     List<CategoryResponse> getSubCategories(UUID parentId);
 
+    List<CategoryResponse> getAllCategories();
+
     CategoryResponse getById(UUID id);
 
     Category findById(UUID id);
