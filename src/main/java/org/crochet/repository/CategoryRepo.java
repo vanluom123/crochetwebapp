509aab0ee0467ba769e40b8c4a8b26c3495bed7f
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-24 15:41:45 +0700

    Refactor CategoryRepo queries

--- src/main/java/org/crochet/repository/CategoryRepo.java
+++ src/main/java/org/crochet/repository/CategoryRepo.java
@@ -8,9 +8,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepo extends JpaRepository<Category, UUID> {
-    @Query("select c.id from Category c where c.name = ?1")
+    @Query("select c from Category c where c.name = ?1")
     Optional<Category> findByName(String name);
 
     @Query("select (count(c.id) > 0) from Category c where c.name = ?1 and c.parent is null")
     boolean existsByNameAndParentIsNull(String name);
+
+    @Query("select (count(c.id) > 0) from Category c where c.name = ?1 and c.parent is not null")
+    boolean existsByNameAndParentIsNotNull(String name);
 }
