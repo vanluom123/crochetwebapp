79f5cb721136b6a75ee60485ee5b7ac4a99f36c3
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-24 14:39:54 +0700

    Add custom queries

--- src/main/java/org/crochet/repository/OrderPatternDetailRepository.java
+++ src/main/java/org/crochet/repository/OrderPatternDetailRepository.java
@@ -2,10 +2,12 @@ package org.crochet.repository;
 
 import org.crochet.model.OrderPatternDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderPatternDetailRepository extends JpaRepository<OrderPatternDetail, UUID> {
+    @Query("select o.id, o.orderDate, o.status from OrderPatternDetail o where o.transactionId = ?1")
     Optional<OrderPatternDetail> findByTransactionId(String transactionId);
 }
