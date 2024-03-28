5c2117538f19c57fcf8be46e61f25ae2efb6f808
Charles Phan <phanvanluom97bd@gmail.com>
2024-03-28 21:46:18 +0700

    Fix the issue with the currency code not being displayed on UI.

--- src/main/java/org/crochet/payload/response/ProductResponse.java
+++ src/main/java/org/crochet/payload/response/ProductResponse.java
@@ -1,5 +1,6 @@
 package org.crochet.payload.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,6 +13,8 @@ public class ProductResponse {
     private String name;
     private String description;
     private double price;
+    @JsonProperty("currency_code")
+    private String currencyCode;
     private Set<FileResponse> files;
     private CategoryResponse category;
 }
