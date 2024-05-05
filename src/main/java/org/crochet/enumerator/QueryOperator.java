08559d354f3f15d13bdc04cd3359d6131f7a0e93
Charles Phan <phanvanluom97bd@gmail.com>
2024-05-06 01:56:32 +0700

    Fixing search (#101)
    If lowercase characters are entered, results are not displayed.

--- src/main/java/org/crochet/enumerator/QueryOperator.java
+++ src/main/java/org/crochet/enumerator/QueryOperator.java
@@ -4,7 +4,9 @@ import java.util.EnumSet;
 
 public enum QueryOperator {
     GREATER_THAN,
+    GREATER_THAN_OR_EQUALS,
     LESS_THAN,
+    LESS_THAN_OR_EQUALS,
     EQUALS,
     LIKE,
     NOT_EQUALS,
