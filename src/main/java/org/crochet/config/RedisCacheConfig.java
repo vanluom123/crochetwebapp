fe281e5fd2d70f5cdf8679a625914f6926928e6c
Charles Phan <phanvanluom97bd@gmail.com>
2024-05-10 20:31:04 +0700

    Add redis configuration and update other ones

---
+++ src/main/java/org/crochet/config/RedisCacheConfig.java
@@ -0,0 +1,91 @@
+package org.crochet.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.crochet.properties.RedisCacheProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "redis")
+    public RedisCacheProperties redisCacheProperties() {
+        return new RedisCacheProperties();
+    }
+
+    public ObjectMapper objectMapper(){
+        return JsonMapper
+                .builder()
+                .addModule(new JavaTimeModule())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                .configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
+                .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false)
+                .activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                        ObjectMapper.DefaultTyping.EVERYTHING)
+                .visibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+                .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisCacheTemplate() {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        RedisSerializer<Object> valueSerializer = RedisSerializer.json();
+
+        template.setKeySerializer(keySerializer);
+        template.setValueSerializer(valueSerializer);
+
+        template.setHashKeySerializer(keySerializer);
+        template.setHashValueSerializer(valueSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager() {
+        RedisSerializer<Object> serializer = new GenericJackson2JsonRedisSerializer(objectMapper());
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+                .disableCachingNullValues();
+        return RedisCacheManager.builder(jedisConnectionFactory())
+                .cacheDefaults(config)
+                .transactionAware()
+                .build();
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration conf = new RedisStandaloneConfiguration();
+        conf.setHostName(redisCacheProperties().getHost());
+        conf.setPort(redisCacheProperties().getPort());
+        conf.setUsername(redisCacheProperties().getUsername());
+        conf.setPassword(redisCacheProperties().getPassword());
+        return new JedisConnectionFactory(conf);
+    }
+}
