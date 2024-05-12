1565b564d432a988b9f10d2760f5640ad702f895
Charles Phan <phanvanluom97bd@gmail.com>
2024-05-12 21:17:44 +0700

    Update custom user details service
    * Remove @Transactional
    * Remove loadUserById function. It's unnecessary

--- src/main/java/org/crochet/security/CustomUserDetailsService.java
+++ src/main/java/org/crochet/security/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package org.crochet.security;
 
 
-import org.crochet.exception.ResourceNotFoundException;
 import org.crochet.model.User;
 import org.crochet.repository.UserRepository;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,10 +9,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * CustomUserDetailsService class
@@ -41,7 +40,6 @@ public class CustomUserDetailsService implements UserDetailsService {
      * @throws UsernameNotFoundException If the user is not found for the given email.
      */
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         // Retrieve the user by email from the UserRepository
         User user = userRepository.findByEmail(email)
@@ -53,40 +51,14 @@ public class CustomUserDetailsService implements UserDetailsService {
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
 
         // Create a UserPrincipal object from the retrieved user
-        return UserPrincipal.builder()
+        var userDetails = UserPrincipal.builder()
                 .id(user.getId())
                 .email(user.getEmail())
                 .password(user.getPassword())
                 .authorities(authorities)
+                .attributes(Map.of("name", email))
                 .build();
-    }
 
-
-    /**
-     * Loads user details based on the provided user ID.
-     *
-     * @param id The ID of the user.
-     * @return The UserDetails object representing the user.
-     * @throws ResourceNotFoundException If the user is not found for the given ID.
-     */
-    @Deprecated
-    @Transactional
-    public UserDetails loadUserById(String id) {
-        // Retrieve the user by ID from the UserRepository
-        User user = userRepository.findById(id)
-                .filter(User::isEmailVerified)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with %s not found", id)));
-
-        // Create a list of GrantedAuthority with a single authority ROLE_USER
-        List<GrantedAuthority> authorities =
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
-
-        // Create a UserPrincipal object from the retrieved user
-        return UserPrincipal.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .password(user.getPassword())
-                .authorities(authorities)
-                .build();
+        return userDetails;
     }
 }
