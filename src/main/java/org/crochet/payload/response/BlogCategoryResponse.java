30b057ec29a2ab99dfaa8bc2e108d60d17ce743f
Charles Phan <phanvanluom97bd@gmail.com>
2024-05-19 01:36:24 +0700

    Blog category

--- src/main/java/org/crochet/payload/response/BlogCategoryResponse.java
+++ src/main/java/org/crochet/payload/response/BlogCategoryResponse.java
@@ -1,5 +1,6 @@
 package org.crochet.payload.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BlogCategoryResponse implements Serializable {
     private String id;
     private String name;
