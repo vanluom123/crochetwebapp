db4a73ca91d42527a8d04713b1f6a66e2211b32a
Charles Phan <phanvanluom97bd@gmail.com>
2024-06-01 10:03:33 +0700

    Update cacheable (#117)
    * Add log
    * Remove @Cacheable at home service
    * Add @Cacheable for services

--- src/main/java/org/crochet/service/impl/CacheEvictor.java
+++ src/main/java/org/crochet/service/impl/CacheEvictor.java
@@ -1,9 +1,11 @@
 package org.crochet.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class CacheEvictor {
     private final RedisCacheManager cacheManager;
@@ -14,9 +16,12 @@ public class CacheEvictor {
 
     @Scheduled(cron = "0 0 4 * * ?")
     public void evictAllCachesAt4AM() {
+        log.info("Evicting all caches at 4 AM");
         cacheManager.getCacheNames().forEach(name -> {
+            log.info("Evicting cache: {}", name);
             var cache = cacheManager.getCache(name);
             if (cache != null) {
+                log.info("Clearing cache: {}", name);
                 cache.clear();
             }
         });
