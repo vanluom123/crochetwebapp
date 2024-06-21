16260f69d1bf86f33c8160a6367386007f7b27bd
Charles Phan <phanvanluom97bd@gmail.com>
2024-06-21 10:28:30 +0700

    Fixing CORS when loading PDF (#153)

--- src/main/java/org/crochet/service/HomeService.java
+++ src/main/java/org/crochet/service/HomeService.java
@@ -5,7 +5,5 @@ import org.crochet.payload.response.HomeResponse;
 import java.util.concurrent.CompletableFuture;
 
 public interface HomeService {
-    HomeResponse getHomes();
-
     CompletableFuture<HomeResponse> getHomesAsync();
 }
