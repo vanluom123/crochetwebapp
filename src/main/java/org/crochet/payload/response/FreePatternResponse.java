83eba183790000a336be48d8090df37e0b2d6eaa
Charles Phan <phanvanluom97bd@gmail.com>
2024-07-15 15:56:07 +0700

    Update specification with filters

--- src/main/java/org/crochet/payload/response/FreePatternResponse.java
+++ src/main/java/org/crochet/payload/response/FreePatternResponse.java
@@ -1,17 +1,14 @@
 package org.crochet.payload.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.crochet.constant.AppConstant;
 import org.crochet.enumerator.ChartStatus;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -31,7 +28,4 @@ public class FreePatternResponse implements Serializable {
     private ChartStatus status;
     private List<FileResponse> images;
     private List<FileResponse> files;
-    @JsonFormat(pattern = AppConstant.DATE_PATTERN)
-    private LocalDateTime createdDate;
-
 }
