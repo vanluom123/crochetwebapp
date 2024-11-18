2932c35671dfa0e4f288f4f82c23e485505e2c67
tieuphuongcrochet <117068991+tieuphuongcrochet@users.noreply.github.com>
2024-11-18 21:18:28 +0700

    Fixing Redis Recursion (#211)
    * Update gradle version
    
    * Remove @RequiredArgsConstructor
    
    * ObjectMapper configuration

--- src/main/java/org/crochet/config/RedisCacheConfig.java
+++ src/main/java/org/crochet/config/RedisCacheConfig.java
@@ -1,5 +1,13 @@
 package org.crochet.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.crochet.properties.RedisCacheProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +27,22 @@ public class RedisCacheConfig {
     @ConfigurationProperties(prefix = "redis")
     public RedisCacheProperties redisCacheProperties() {
         return new RedisCacheProperties();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper
+                .builder()
+                .addModule(new JavaTimeModule())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                .configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false)
+                .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false)
+                .activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                        ObjectMapper.DefaultTyping.NON_FINAL)
+                .visibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+                .build();
     }
 
     @Bean
