a1a7e4c4511765b4689c0e7f78b327dbb148e093
Hung Duong <sang.nn7@oude.edu.vn>
2025-02-09 19:20:26 +0700

    Fixing shared collection reference (#240)
    * Enable toBuilder option in SuperBuilder annotation for BaseEntity
    
    * Allow both ADMIN and USER roles to access create, update, and delete category endpoints
    
    * Refactor FreePatternMapper and FreePatternServiceImpl to streamline update logic for FreePattern entities
    
    * Refactor mappers to implement partial update logic for BlogPost, Pattern, and Product entities

--- src/main/java/org/crochet/mapper/PatternMapper.java
+++ src/main/java/org/crochet/mapper/PatternMapper.java
@@ -3,6 +3,7 @@ package org.crochet.mapper;
 import org.crochet.model.Pattern;
 import org.crochet.payload.request.PatternRequest;
 import org.crochet.payload.response.PatternResponse;
+import org.crochet.util.ImageUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -21,6 +22,43 @@ public interface PatternMapper extends PartialUpdate<Pattern, PatternRequest> {
     @Mapping(target = "isHome", source = "home")
     @Mapping(target = "currencyCode", source = "currencyCode")
     PatternResponse toResponse(Pattern pattern);
+
+    @Override
+    default Pattern partialUpdate(PatternRequest req, Pattern pattern) {
+        if (pattern == null) {
+            return null;
+        }
+        if (req.getName() != null) {
+            pattern.setName(req.getName());
+        }
+        if (req.getDescription() != null) {
+            pattern.setDescription(req.getDescription());
+        }
+        if (req.getPrice() != pattern.getPrice()) {
+            pattern.setPrice(req.getPrice());
+        }
+        if (req.getCurrencyCode() != null) {
+            pattern.setCurrencyCode(req.getCurrencyCode());
+        }
+        if (req.isHome() != pattern.isHome()) {
+            pattern.setHome(req.isHome());
+        }
+        if (req.getLink() != null) {
+            pattern.setLink(req.getLink());
+        }
+        if (req.getContent() != null) {
+            pattern.setContent(req.getContent());
+        }
+        if (req.getImages() != null && !req.getImages().isEmpty()) {
+            var sortedImages = ImageUtils.sortFiles(req.getImages());
+            pattern.setImages(FileMapper.INSTANCE.toEntities(sortedImages));
+        }
+        if (req.getFiles() != null && !req.getFiles().isEmpty()) {
+            var sortedFiles = ImageUtils.sortFiles(req.getFiles());
+            pattern.setFiles(FileMapper.INSTANCE.toSetEntities(sortedFiles));
+        }
+        return pattern;
+    }
 
     List<PatternResponse> toResponses(Collection<Pattern> patterns);
 }
--- src/main/java/org/crochet/model/BaseEntity.java
+++ src/main/java/org/crochet/model/BaseEntity.java
@@ -13,7 +13,7 @@ import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
-@SuperBuilder
+@SuperBuilder(toBuilder = true)
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
