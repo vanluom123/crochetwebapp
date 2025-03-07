915e6c5770f35efb571442a56651a3f7924c1401
Phan Văn Lượm <phanvanluom97bd@gmail.com>
2025-03-08 04:36:33 +0700

    Refactor entity retrieval with Specification filtering, enhance pagination, and delete GenericFilter.
    - Delete GenericFilter
    - Refactor specifications to suppress warnings and improve code clarity
    - Refactor Pattern retrieval to use Specification for filtering and enhance pagination capabilities
    - Refactor Product retrieval to use Specification for filtering and update pagination endpoint to GET
    - Refactor BlogPost retrieval to use Specification for filtering and update pagination endpoint to GET
    - Refactor user retrieval to use Specification for filtering and update pagination endpoint to GET
    - Add filtering capabilities to FreePattern endpoints and introduce ObjectUtils for common operations

--- src/main/java/org/crochet/repository/PatternSpecifications.java
+++ src/main/java/org/crochet/repository/PatternSpecifications.java
@@ -1,9 +1,14 @@
 package org.crochet.repository;
 
 import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
+import org.crochet.model.Category;
 import org.crochet.model.Pattern;
 import org.springframework.data.jpa.domain.Specification;
 
+@SuppressWarnings({"DataFlowIssue", "unused"})
 public class PatternSpecifications {
 
     public static Specification<Pattern> fetchJoin() {
@@ -14,6 +19,22 @@ public class PatternSpecifications {
             }
             q.distinct(true);
             return cb.conjunction();
+        };
+    }
+
+    public static Specification<Pattern> inHierarchy(String categoryId) {
+        return (r, q, cb) -> {
+            Subquery<String> subquery = q.subquery(String.class);
+            Root<Category> subRoot = subquery.from(Category.class);
+
+            Predicate or = cb.or(cb.equal(subRoot.get("id"), categoryId),
+                    cb.equal(subRoot.get("parent").get("id"), categoryId));
+            subquery.select(subRoot.get("id"))
+                    .where(or);
+
+            q.distinct(true);
+
+            return cb.in(r.get("category").get("id")).value(subquery);
         };
     }
 }
--- src/main/java/org/crochet/service/PatternService.java
+++ src/main/java/org/crochet/service/PatternService.java
@@ -1,16 +1,17 @@
 package org.crochet.service;
 
-import org.crochet.payload.request.Filter;
+import org.crochet.model.Pattern;
 import org.crochet.payload.request.PatternRequest;
 import org.crochet.payload.response.PatternPaginationResponse;
 import org.crochet.payload.response.PatternResponse;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
 public interface PatternService {
     void createOrUpdate(PatternRequest request);
 
-    PatternPaginationResponse getPatterns(int pageNo, int pageSize, String sortBy, String sortDir, Filter[] filters);
+    PatternPaginationResponse getPatterns(int pageNo, int pageSize, String sortBy, String sortDir, String categoryId, Specification<Pattern> spec);
 
     List<PatternResponse> getLimitedPatterns();
 
